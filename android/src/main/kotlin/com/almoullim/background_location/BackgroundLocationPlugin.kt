73262d2d6df15a49357af95f2a55ec6a0eb13360
vvlado23 <43526651+vvlado23@users.noreply.github.com>
2025-02-19 12:06:02 +0200

    removes legacy code

--- android/src/main/kotlin/com/almoullim/background_location/BackgroundLocationPlugin.kt
+++ android/src/main/kotlin/com/almoullim/background_location/BackgroundLocationPlugin.kt
@@ -13,13 +13,14 @@ class BackgroundLocationPlugin : FlutterPlugin, ActivityAware {
 
         /**
         Legacy for v1 embedding
-         */
+         
         @SuppressWarnings("deprecation")
         fun registerWith(registrar: PluginRegistry.Registrar) {
             val service = BackgroundLocationService.getInstance()
             service.onAttachedToEngine(registrar.context(), registrar.messenger())
             registrar.addRequestPermissionsResultListener(service)
         }
+        */
 
         const val TAG = "com.almoullim.Log.Tag"
         const val PLUGIN_ID = "com.almoullim.background_location"
